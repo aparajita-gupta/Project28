248378cb41b0dfe5ed60a910b087b59fc01e4334
Aparajita Gupta <68521965+aparajita-gupta@users.noreply.github.com>
2021-04-07 13:27:58 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,8 +6,8 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var boyImg,platform;
-var bird, slingShot;
-var mango,mango1,mango2,mango3,mango4;
+var rock, slingShot;
+var mango,mango1,mango2,mango3,mango4, mango5, mango6;
 var treeIMG,tree;
 function preload(){
 
@@ -19,17 +19,19 @@ var boyIMG,boy;
 
 function setup(){
     var canvas = createCanvas(1200,800);
+
     engine = Engine.create();
     world = engine.world;
 
     ground = new Ground(600,height,1200,20);
-    
 
-    mango = new Mango(900,300,30);
-    mango1 = new Mango(850,250,30);
-    mango2 = new Mango(910,370,30);
-    mango3 = new Mango(800,400,30);
-    mango4 = new Mango(1000,400,30);
+    mango = new Mango(910,310,30);
+    mango1 = new Mango(860,260,30);
+    mango2 = new Mango(920,380,30);
+    mango3 = new Mango(810,410,30);
+    mango4 = new Mango(1010,410,30);
+    mango5 = new Mango(920,280,30);
+    mango6 = new Mango(880,230,30);
 
 boy = createSprite(250,730,10,10);
 tree = createSprite(900,500,20,20);
@@ -38,13 +40,13 @@ tree.addImage("Tree",treeIMG);
 boy.addImage("boy1",boyIMG);
   boy.scale = 0.1;
   tree.scale = 0.5;
-bird = new Stone(250,730,25);
+rock = new Stone(250,730,25);
     
-slingShot = new SlingShot(bird.body, {x:200,y:670});
+slingShot = new SlingShot(rock.body, {x:200,y:670});
 }
 
 function mouseDragged(){
-Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
+Matter.Body.setPosition(rock.body,{x:mouseX,y:mouseY});
 }
 
 function mouseReleased(){
@@ -76,17 +78,23 @@ ground.display();
     mango2.display();
     mango3.display();
     mango4.display();
-    bird.display();
-    detectCollision(bird,mango);
-    detectCollision(bird,mango1);
-    detectCollision(bird,mango2);
-    detectCollision(bird,mango3);
-    detectCollision(bird,mango4);
+    mango5.display();
+    mango6.display();
+
+    rock.display();
+
+    detectCollision(rock,mango);
+    detectCollision(rock,mango1);
+    detectCollision(rock,mango2);
+    detectCollision(rock,mango3);
+    detectCollision(rock,mango4);
+    detectCollision(rock,mango5);
+    detectCollision(rock,mango6);
 }
 
 function  keyPressed(){
     if(keyCode === 32){
 
-slingShot.attach(bird.body);
+slingShot.attach(rock.body);
     }
 }
