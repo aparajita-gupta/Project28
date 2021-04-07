0575ac4cf8716dd02973f3dd8a081404e8c05a30
Aparajita Gupta <68521965+aparajita-gupta@users.noreply.github.com>
2021-04-07 13:29:09 -0700

    Update SlingShot.js

--- SlingShot.js
+++ SlingShot.js
@@ -6,32 +6,24 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
-        this.pointB=pointB;
-       // this.bodyA=body;
+        this.pointB = pointB;
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
     }
 
     fly(){
 this.sling.bodyA=null;
-
     }
 
 attach(body){
 this.sling.bodyA=body;
-
 }
-
     display(){
-        if(this.sling.bodyA){
-
-
-        
+        if(this.sling.bodyA){       
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
             strokeWeight(4);
             line(pointA.x, pointA.y, pointB.x, pointB.y);
         }
     }
-    
 }
